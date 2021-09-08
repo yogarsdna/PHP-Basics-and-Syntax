137056441869d07b9a2ab150d1aa1aece9146d15
yogarsdna <gedeyoga54@gmail.com>
2021-09-08 22:02:34 +0700

    add a website shortcut

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
     <button type = "button" class = "btn btn-primary">Click Me!</button>
     <button type = "button" class = "btn btn-secondary">Click Me!</button>
     <button type = "button" class = "btn btn-info">Click Me!</button>
+    <a href = "https://www.heroku.com" class = "btn btn-danger" >Heroku.com</a>
 
 <?php
     require 'include/footer.php';
